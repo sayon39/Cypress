d1080d827bf84312280bb7e6991f5d466d22b60b
sayondas <sayon39@gmail.com>
2023-09-17 21:30:43 -0400

    Including Report Config

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,17 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+    reporter: 'cypress-mochawesome-reporter',
+    video: false,
+    reporterOptions: {
+    charts: true,
+    reportPageTitle: 'Cypress Inline Reporter',
+    embeddedScreenshots: true, 
+    inlineAssets: true, //Adds the asserts inline
+  },
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
